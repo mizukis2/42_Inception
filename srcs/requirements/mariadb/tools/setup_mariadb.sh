2dc78d3f6baf8f4c378f2779b90fb5c0be933a44
Mizuki Matsui <mmatsui@f1r1s6.codam.nl>
2026-08-03 15:20:10 +0200

    update

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+echo "Starting MariaDB container"
+
+mkdir -p /run/mysqld
+
+chown -R mysql:mysql /run/mysqld
+
 # 1. Check: has the database already been initialized before?
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    # 2. First run - do the one-time setup
-    #    - initialize the base MariaDB system tables
-    #    - create your wordpress database
-    #    - create your two users
-    #    - set their passwords/privileges
-    echo "First run - initializing database..."
-    # ... your setup commands / .sql script go here
+    mariadb-install-db --user=mysql
+    
+    mariadb --user=mysql &
+    until mysqladmin ping -u root --silent; do
+        sleep 1
+    done
+
+    mysql -u root <<-EOSQL
+        CREATE DATABASE wordpress;
+        CREATE USER 'wp_user'@'%' IDENTIFIED BY 'password';
+        GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';
+    EOSQL
+
+    mysqladmin -u root shutdown
 fi
 
-# 3. Always, every time - actually start the server in the foreground
-exec mysqld
+exec mysqld --user=mysql
+
+
+#database name, user, password will be replaced as env or secret
