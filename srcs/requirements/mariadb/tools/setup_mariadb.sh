e006e51398c52328a8361ea9cba6d1c6e5b4073b
Mizuki Matsui <mmatsui@f1r1s13.codam.nl>
2026-08-04 13:39:50 +0200

    mariadb updated

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
+set -e
+
 echo "Starting MariaDB container"
 
 mkdir -p /run/mysqld
 
 chown -R mysql:mysql /run/mysqld
 
-# 1. Check: has the database already been initialized before?
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql
     
-    mariadb --user=mysql &
+    mysqld --user=mysql --skip-networking &
     until mysqladmin ping -u root --silent; do
         sleep 1
     done
 
     mysql -u root <<-EOSQL
-        CREATE DATABASE wordpress;
-        CREATE USER 'wp_user'@'%' IDENTIFIED BY 'password';
-        GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';
-    EOSQL
+        CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+        CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+        GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+        FLUSH PRIVILEGES;
+EOSQL
 
     mysqladmin -u root shutdown
+
 fi
+
+echo "Starting MariaDB server"
 
 exec mysqld --user=mysql
 
-
-#database name, user, password will be replaced as env or secret
