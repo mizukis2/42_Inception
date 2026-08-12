035c42c76877c3e002d96da1f7516ca6d8d43a82
松井瑞希 <matsuimiki@matsuimikinoMacBook-Pro.local>
2026-08-12 16:26:37 +0200

    update wordpress no test yet

---
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting WordPress container"
+
+echo "Waiting for MariaDB to be ready..."
+until mariadb-admin ping -h mariadb -u root -p"$MYSQL_ROOT_PASSWORD" --silent; do
+    sleep 1
+done
+
+echo "MariaDB is ready!"
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+    wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" \
+        --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb \
+        --path=/var/www/html --allow-root
+
+    wp core install --url=https://mmatsui.42.fr --title=inception \
+        --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --path=/var/www/html --allow-root
+        
+    echo "WordPress is initialized."
+
+else
+    echo "WordPress already initialized. Skipping setup."
+
+fi
+
+echo "Starting PHP-FPM..."
+
+exec php-fpm8.2
