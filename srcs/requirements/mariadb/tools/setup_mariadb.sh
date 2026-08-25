5f50516c98834c280142c0cd183b831a92f21694
Mizuki Matsui <mmatsui@f1r1s13.codam.nl>
2026-08-25 10:35:47 +0200

    fixed mariadb.sh, make EOSQL one tab back (previous one was forwarded)

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -22,7 +22,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     MYSQL_ROOT_PASSWORD="$(cat /run/secrets/db_root_password)"
     mariadb -u root <<-EOSQL
         ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-    EOSQL
+EOSQL
 
     echo "Root password set. Creating database and user..."
 
@@ -33,7 +33,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
         CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
         GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
         FLUSH PRIVILEGES;
-    EOSQL
+EOSQL
 
     mariadb-admin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
